b8e2f65a2a9c1aa02bc9f6bc3660fd1c0a5f707b
Chandan Kumar <ck923182@gmail.com>
2026-01-15 23:06:48 +0530

    Created a all Default User using the seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,14 +9,65 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    // public function run(): void
+    // {
+    //     $admin = User::create([
+    //         'name' => 'Admin User',
+    //         'email' => 'admin@example.com',
+    //         'password' => Hash::make('password'),
+    //     ]);
+
+    //     $admin->assignRole(UserRole::ADMIN->value);
+    // }
     public function run(): void
     {
-        $admin = User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+        // Create an admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            // Directly assign the admin role from the enum
+            'role' => UserRole::ADMIN->value,
         ]);
 
-        $admin->assignRole(UserRole::ADMIN->value);
+        // Create an Student  user  Default
+        User::create([
+            'name' => 'Student',
+            'email' => 'student@gmail.com',
+            'password' => Hash::make('student123'),
+            // The default role 'student' will be applied automatically if not specified
+        ]);
+
+        // Create an COMMUNITY_MEMBER  user
+        User::create([
+            'name' => 'Coummnity Member',
+            'email' => 'communitymember@gmail.com',
+            'password' => Hash::make('comunitymember123'),
+            'role' => UserRole::COMMUNITY_MEMBER->value,
+        ]);
+
+        // Create an COMMUNITY_CHAIR  user
+        User::create([
+            'name' => 'Communitychair',
+            'email' => 'communitychair@gmail.com',
+            'password' => Hash::make('chmmunitychair123'),
+            'role' => UserRole::COMMUNITY_CHAIR->value,
+        ]);
+
+        // Create an PRESIDENT  user
+        User::create([
+            'name' => 'President',
+            'email' => 'president@gmail.com',
+            'password' => Hash::make('president123'),
+            'role' => UserRole::PRESIDENT->value,
+        ]);
+
+        // Create an VICE_PRESIDENT  user
+        User::create([
+            'name' => 'Vice president',
+            'email' => 'vicepresident@gmail.com',
+            'password' => Hash::make('vicepresident123!@#'),
+            'role' => UserRole::VICE_PRESIDENT->value,
+        ]);
     }
 }
